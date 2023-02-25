b8b38df4af87fe7a2316989f945ffc4a4c295be1
ahmedghanam <yghnam334455@gmail.com>
2023-02-25 12:13:11 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:folio/provider/app_provider.dart';
+import 'package:folio/provider/drawer_provider.dart';
+import 'package:folio/provider/scroll_provider.dart';
+import 'package:folio/sections/main/main_section.dart';
+import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
-import 'home/home_screen.dart';
-import 'provider/themeProvider.dart';
-import 'provider/themeStyles.dart';
+import 'package:folio/configs/core_theme.dart' as theme;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
-  runApp(SplachScreen());
+  runApp(const MyApp());
 }
 
-class SplachScreen extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  _SplachScreenState createState() => _SplachScreenState();
+  MyAppState createState() => MyAppState();
 }
 
-class _SplachScreenState extends State<SplachScreen> {
-
-  ThemeProvider _themeProvider = ThemeProvider();
-
-  void getCurrentAppTheme() async {
-    _themeProvider.lightTheme = await _themeProvider.darkThemePref.getTheme();
-  }
-
-  @override
-  void initState() {
-    getCurrentAppTheme();
-    super.initState();
-  }
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ahmed',
-      theme: ThemeStyles.themeData(_themeProvider.lightTheme, context),
-      home: HomeScreenLoading(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppProvider()),
+        ChangeNotifierProvider(create: (_) => DrawerProvider()),
+        ChangeNotifierProvider(create: (_) => ScrollProvider()),
+      ],
+      child: Consumer<AppProvider>(
+        builder: (context, value, _) => MaterialChild(
+          provider: value,
+        ),
+      ),
     );
   }
 }
 
+class MaterialChild extends StatefulWidget {
+  final AppProvider provider;
+  const MaterialChild({Key? key, required this.provider}) : super(key: key);
+
+  @override
+  State<MaterialChild> createState() => _MaterialChildState();
+}
+
+class _MaterialChildState extends State<MaterialChild> {
+  void initAppTheme() {
+    final appProviders = AppProvider.state(context);
+    appProviders.init();
+  }
+
+  @override
+  void initState() {
+    initAppTheme();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hamza',
+      theme: theme.themeLight,
+      darkTheme: theme.themeDark,
+      themeMode: widget.provider.themeMode,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const MainPage(),
+      },
+    );
+  }
+}
